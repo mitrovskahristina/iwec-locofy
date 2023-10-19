a75201bfec9f7b9431e31cfcaf80d735ae93b5f2
mitrovskahristina <mitrovskahristina>
2023-10-19 08:23:11 +0000

    Pushed from Locofy Builder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,47 +1,58 @@
 import styled from "styled-components";
 
-const PlaylistAddCheckIcon = styled.img`
+const Groups3Icon = styled.img`
   position: relative;
   width: 1.13rem;
   height: 1.13rem;
 `;
-const Presence = styled.div`
+const StudentsAndTeachers = styled.div`
   position: relative;
   font-weight: 600;
   @media screen and (max-width: 960px) {
     display: none;
   }
 `;
-const PlaylistAddCheckParentRoot = styled.a`
-  text-decoration: none;
+const Groups3ParentRoot = styled.a`text-decoration: none;
+  align-self: stretch;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  padding: var(--padding-3xs) var(--padding-95xl) var(--padding-3xs)
-    var(--padding-xl);
+  padding: var(--padding-3xs) var(--padding-25xl) var(--padding-3xs) var(--padding-xl);
   gap: var(--gap-3xs);
   text-align: left;
   font-size: var(--typography-caption-size);
   color: var(--color-darkgray);
   font-family: var(--font-lato);
   @media screen and (max-width: 960px) {
-    padding-left: var(--padding-3xs);
-    padding-right: var(--padding-3xs);
-    box-sizing: border-box;
+  padding-left: var(--padding-3xs);
+  padding-right: var(--padding-3xs);
+  box-sizing: border-box;
+  
   }
-  cursor: ${(p) => p.frameACursor};
+  align-self: ${(p) => p.frameAAlignSelf}
+  padding: ${(p) => p.frameAPadding}
+  cursor: ${(p) => p.frameACursor}
 `;
 
-const Nav = ({ icon, navname, onFrameLinkClick, frameACursor }) => {
+const Nav = ({
+  frameAAlignSelf,
+  frameAPadding,
+  groups3,
+  studentsAndTeachers,
+  onFrameLinkClick,
+  frameACursor,
+}) => {
   return (
-    <PlaylistAddCheckParentRoot
+    <Groups3ParentRoot
+      frameAAlignSelf={frameAAlignSelf}
+      frameAPadding={frameAPadding}
       onClick={onFrameLinkClick}
       frameACursor={frameACursor}
     >
-      <PlaylistAddCheckIcon alt="" src={icon} />
-      <Presence>{navname}</Presence>
-    </PlaylistAddCheckParentRoot>
+      <Groups3Icon alt="" src={groups3} />
+      <StudentsAndTeachers>{studentsAndTeachers}</StudentsAndTeachers>
+    </Groups3ParentRoot>
   );
 };
 
